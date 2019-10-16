38050a7cf33b2118d68d61c79514b50234c7c324
Naveen Kumar <navuniverse@gmail.com>
2019-10-17 00:28:01 +0530

    Removed unused annotations

--- src/main/java/com/rockingengineering/spring/redis/SpringBootRedisApplication.java
+++ src/main/java/com/rockingengineering/spring/redis/SpringBootRedisApplication.java
@@ -1,13 +1,9 @@
 package com.rockingengineering.spring.redis;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
-@EnableCaching
 @SpringBootApplication
-@EnableAutoConfiguration
 public class SpringBootRedisApplication {
 
 	public static void main(String[] args) {
